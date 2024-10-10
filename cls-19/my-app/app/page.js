58e42c7f5caa786c3d2e60c115e6e0b4220ab20c
wessam aftab <wisamkayani360@gmail.com>
2024-10-10 15:01:03 +0500

    ok
    ok

--- cls-19/my-app/app/page.js
+++ cls-19/my-app/app/page.js
@@ -1,9 +1,42 @@
+"use client";
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+  const [count, setCount] = React.useState(0);
+  const [data, setData] = React.useState([
+    { text: `hello1` },
+    { text: `hello2` },
+    { text: `hello3` },
+    { text: `hello4` },
+    { text: `hello5` },
+  ]);
+
+  const clicker = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const deletion = (i) => {
+    const filteredData = data.filter((_, index) => index !== i);
+    setData(filteredData);  // Update state so the component re-renders
+  };
+
+  React.useEffect(() => {
+    clicker(); // Initial increment
+  }, []);
+
   return (
     <div>
-      i am page
+      <button onClick={() => clicker()} className="bg-red-500 w-10 h-10">
+        {count}
+      </button>
+      <div className="text-center">
+        {data.map((t, i) => (
+          <div key={i} onClick={() => deletion(i)}>
+            {t.text}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
